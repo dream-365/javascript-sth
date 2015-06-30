05162a671b90fd9b884c3e8fa5ae727c4228bf68
dream-365 <support-jec@hotmail.com>
2015-06-30 10:25:00 +0800

    angular-winjs listview sample

--- grunt-tasks/basic/client/app.js
+++ grunt-tasks/basic/client/app.js
@@ -1,4 +1,51 @@
+/// <reference path="../../../typings/angularjs/angular.d.ts"/>
+
+var vroot = 'http://try.buildwinjs.com';
+
 angular.module('sample', ['winjs']).controller("sampleController", function ($scope) {
-    $scope.rating = 1;
-    $scope.maxRating = 5;
+    var Mode = {
+        editMode: {
+            text: "edit",
+            selectionMode: "multi",
+            tapBehavior: "toggleSelect"
+        },
+        readOnly: {
+            text: "readonly",
+            selectionMode: "none",
+            tapBehavior: "none"
+        }
+    };
+
+    $scope.items = [
+        { title: "Marvelous Mint", text: "Gelato", picture: vroot + "/images/fruits/60Mint.png" },
+        { title: "Succulent Strawberry", text: "Sorbet", picture: vroot + "/images/fruits/60Strawberry.png" },
+        { title: "Banana Blast", text: "Low-fat frozen yogurt", picture: vroot + "/images/fruits/60Banana.png" },
+        { title: "Lavish Lemon Ice", text: "Sorbet", picture: vroot + "/images/fruits/60Lemon.png" },
+        { title: "Creamy Orange", text: "Sorbet", picture: vroot + "/images/fruits/60Orange.png" },
+        { title: "Very Vanilla", text: "Ice Cream", picture: vroot + "/images/fruits/60Vanilla.png" },
+        { title: "Banana Blast", text: "Low-fat frozen yogurt", picture: vroot + "/images/fruits/60Banana.png" },
+        { title: "Lavish Lemon Ice", text: "Sorbet", picture: vroot + "/images/fruits/60Lemon.png" }
+    ];
+
+    $scope.addItem = function () {
+        $scope.items.splice(0, 0, {
+            title: "Marvelous Mint",
+            text: "Gelato",
+            picture: vroot + "/images/fruits/60Mint.png"
+        });
+    };
+
+    $scope.removeItem = function () {
+        // Remove the items that are selected
+        for (var i = $scope.selection.length - 1; i > -1; i--) {
+            $scope.items.splice($scope.selection[i], 1);
+        }
+    };
+
+    $scope.toggleEditMode = function () {
+        $scope.mode = ($scope.mode === Mode.readOnly) ? Mode.editMode : Mode.readOnly;
+    };
+
+    $scope.mode = Mode.readOnly;
+    $scope.selection = [];
 });
